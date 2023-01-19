6d7a980392ba4fce0aced36c1952f1330471710b
Venkatesh nellore <nellorevenky007@gmail.com>
2023-01-19 13:05:54 +0530

    updated statement

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','Post'])
 def index():
-    return 'CI/CD Pipeline has been established'
+    return 'CI/CD Pipeline has been Constructedgi'
 
 
 if __name__ == "__main__":
